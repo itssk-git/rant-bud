6ac5b921ff77b77dcfefe3b4cf97f3b63aeaece9
Sachin Karki <itssachin079@gmail.com>
2024-12-23 17:59:27 +0545

    Login and Logout implemented

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import main,home,room,create_room,update_room
+from .views import home,room,create_room,update_room,deleteRoom,loginPage,logoutUser
 
 urlpatterns = [
-    path("home/", home, name="home" ),
+    path("login/",loginPage,name="login"),
+    path("logout/",logoutUser,name="logout"),
+    path("", home, name="home" ),
     path("room/<str:pk>/", room, name="room" ),
     path("create-room/", create_room, name="create-room"),
     path("update-room/<str:pk>", update_room, name="update-room"),
+    path("delete/<str:pk>", deleteRoom, name="delete-room"),
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -1,13 +1,52 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from django.contrib import messages
+from django.db.models import Q
+from .models import Room,Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
-def main(request):
-    return render(request,"main.html")
+
+def loginPage(request):
+
+    if request.method== 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        try:
+            user=User.objects.get(username=username)
+        except:
+            messages.error(request,"User doesnt exist")
+        user= authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"Invalid Credentials")
+
+
+
+
+    context={}
+    return render(request,'firstApp/login_register.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
 def home(request):
-    rooms=Room.objects.all()
-    context={"rooms":rooms}
+    q=request.GET.get('q') if request.GET.get('q')!=None else ''
+    
+    rooms=Room.objects.filter(Q(topic__name__icontains=q) |
+                              Q(name__icontains=q)|
+                              Q(description__icontains=q)
+                              )
+    topics=Topic.objects.all()
+    room_counts=rooms.count()
+    
+
+    context={"rooms":rooms,"topics": topics,"room_counts":room_counts}
     return render(request,"firstApp/home.html",context=context)
 def room(request,pk):
     room=Room.objects.get(id=int(pk))
@@ -31,11 +70,19 @@ def update_room(request,pk):
     form=RoomForm(instance=room)
 
     if request.method=="POST":
-        form=RoomForm(request.POST)
+        form=RoomForm(request.POST,instance=room)
         if form.is_valid():
             form.save(commit=True)
             return redirect('home')
 
     context={'form': form}
     return render(request,'firstApp/room_form.html',context)
+
+
+def deleteRoom(request,pk):
+    room =Room.objects.get(id=pk)
+    if request.method=="POST":
+        room.delete()
+        return redirect('home')
+    return render(request,'firstApp/delete.html',{'obj':room})
 
